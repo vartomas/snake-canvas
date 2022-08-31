da3c0db8fd8a89b211e99bbe01524d3ac44b65d3
Tomas <tomasvareika85@gmail.com>
2022-08-31 23:16:39 +0300

    eat points

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -6,6 +6,7 @@ interface Settings {
   speed: number;
   direction: 'up' | 'down' | 'left' | 'right';
   snakeColor: string;
+  pointColor: string;
 }
 
 type Positions = [number, number][];
@@ -14,16 +15,33 @@ export const useGame = (canvasRef: React.MutableRefObject<HTMLCanvasElement | nu
   const settings: Settings = {
     gridSize: 40,
     tileSize: 10,
-    speed: 5,
+    speed: 10,
     direction: 'right',
     snakeColor: '#691a1a',
+    pointColor: '#34eb46',
   };
 
   let snakeLength = 5;
 
   const getStartingPosition = (): Positions => [[(settings.gridSize * settings.tileSize) / 2, (settings.gridSize * settings.tileSize) / 2]];
 
+  const generateRandomPointPosition = (): [number, number] => {
+    const randomPos: [number, number] = [
+      Math.floor(Math.random() * settings.gridSize) * settings.tileSize,
+      Math.floor(Math.random() * settings.gridSize) * settings.tileSize,
+    ];
+
+    console.log(randomPos);
+
+    if (positions.find((x) => x[0] === randomPos[0] && x[1] === randomPos[1])) {
+      return generateRandomPointPosition();
+    }
+
+    return randomPos;
+  };
+
   let positions: Positions = getStartingPosition();
+  let pointPosition = generateRandomPointPosition();
 
   const getNewHeadPosition = (): [number, number] => {
     switch (settings.direction) {
@@ -40,16 +58,28 @@ export const useGame = (canvasRef: React.MutableRefObject<HTMLCanvasElement | nu
     }
   };
 
+  const eatPoint = () => {
+    ++snakeLength;
+    pointPosition = generateRandomPointPosition();
+  };
+
   const moveSnake = () => {
     const cutEnd = (arr: [number, number][]) => (snakeLength >= positions.length ? arr : arr.slice(0, snakeLength - positions.length));
     const newPos = getNewHeadPosition();
+
+    if (newPos[0] === pointPosition[0] && newPos[1] === pointPosition[1]) {
+      eatPoint();
+    }
+
     positions = [newPos, ...cutEnd(positions)];
   };
 
   const draw = (ctx: CanvasRenderingContext2D) => {
-    ctx.fillStyle = settings.snakeColor;
+    ctx.fillStyle = settings.pointColor;
     ctx.beginPath();
     ctx.clearRect(0, 0, 500, 500);
+    ctx.fillRect(pointPosition[0], pointPosition[1], settings.tileSize, settings.tileSize);
+    ctx.fillStyle = settings.snakeColor;
     positions.forEach((x) => {
       ctx.fillRect(x[0], x[1], settings.tileSize, settings.tileSize);
     });
