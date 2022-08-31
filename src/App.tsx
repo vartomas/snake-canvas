88618a7afe4e04ab6da3c0dfd01ee9d1aa27d9b0
Tomas <tomasvareika85@gmail.com>
2022-08-31 20:58:20 +0300

    styled, reset, canvas comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,29 @@
-const App = () => <></>;
+import styled from 'styled-components';
+import Game from './components/Game';
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const App = () => (
+  <Wrap>
+    <GlobalStyle />
+    <Game />
+  </Wrap>
+);
+
+const Wrap = styled.div`
+  min-height: 100vh;
+  min-width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default App;
---
+++ src/components/Game.tsx
@@ -0,0 +1,5 @@
+const Game = () => {
+  return <canvas id="game" width="500" height="500" />;
+};
+
+export default Game;
